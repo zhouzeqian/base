887c56957f749f5239a259ce7dd3090872dfb84a
Administrator <Administrator@DESKTOP-6HDLIR7.lan>
2017-09-10 10:12:03 +0800

    替换配置文件

--- base/src/main/java/com/hzlh/Swagger2.java
+++ base/src/main/java/com/hzlh/Swagger2.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
